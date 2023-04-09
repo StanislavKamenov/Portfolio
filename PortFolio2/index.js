387654b03da584388170b474b11bfea0e1fec165
AstrO <76092821+StanislavKamenov@users.noreply.github.com>
2023-04-09 22:01:53 +0300

    Some changes
    Add some stuff

--- PortFolio2/index.js
+++ PortFolio2/index.js
@@ -16,3 +16,17 @@ window.addEventListener("load", () => {
     });
   });
   
+  // Scroll Reveal //
+ScrollReveal ({
+  reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.information, .about, .contactus', {origin: 'top'})
+
+
+
+
+
